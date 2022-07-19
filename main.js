ac4d1b6761e8dbbe7aba4b9400bcb3fc4e43f56f
Emmanuel Essien <ehmaessien@gmail.com>
2022-07-19 11:59:31 +0100

    Update file

--- main.js
+++ main.js
@@ -2,14 +2,27 @@ const getheader = document.querySelector('header');
 const getmenu = document.querySelector('.menu-icon');
 const getclose = document.querySelector('nav ul .closeclass');
 const getli = document.querySelectorAll('nav ul .linkclass');
+console.log(getli)
 getmenu.addEventListener('click', () => {
   getheader.classList.toggle('active');
+  console.log(getheader)
 });
 getclose.addEventListener('click', () => {
   getheader.classList.remove('active');
+  console.log(getheader)
 });
-getli.forEach((item) => {
-  item.addEventListener('click', () => {
+getli.forEach((li) => {
+  li.addEventListener('click', () => {
     getheader.classList.remove('active');
   });
 });
+
+const data = [
+  {
+    id: 1,
+    image:'',
+    title:'',
+    tags: 'Javascript, PHP, HTML'
+
+  }
+]
